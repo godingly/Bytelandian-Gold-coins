691ae01a67a3da6a6ad78b6dc7d1e0f4191f52f7
godingly <godingly@gmail.com>
2017-12-09 18:11:00 +0200

    Create ALPHACODE.c

---
+++ ALPHACODE.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void decode(char* str)
+{
+    unsigned long long w2, w1;
+    w2 = w1 = 1;
+    for (int i = (int) strlen(str) - 2; i >=0 ; i--)
+    {
+    	if (str[i] == '0' || str[i+1] == '0' || str[i+2] == '0') ;
+        else if ((str[i] == '1' && str[i+1] >= '1') || (str[i] == '2' && (str[i+1] <= '6' && str[i+1] >= '1')))
+        {
+            int temp = w1;
+            w1 = w1 + w2;
+            w2 = temp;
+            continue;
+        }
+        w2 = w1;
+    }
+    printf("%llu\n", w1);
+}
+
+int main()
+{
+    char input[5004] = {0};
+
+    while(fgets(input, 5002, stdin) != NULL)
+    {
+        if(input[0] == '0') exit(0);
+        input[strcspn(input, "\n")] = 0;
+        decode(input);
+    }
+}
